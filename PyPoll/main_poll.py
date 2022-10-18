48be55f725b0a63a46d058911432ac59aa41eb4e
Kevin Y <113717031+KYbarra4@users.noreply.github.com>
2022-10-17 23:21:15 -0500

    Add files via upload

---
+++ PyPoll/main_poll.py
@@ -0,0 +1,60 @@
+import os
+import csv
+
+election_csv = os.path.join('..','Resources','election_data.csv')
+
+#votes = []
+#candidates = []
+#total_votes=0
+
+with open(election_csv) as csvfile:
+    csvreader = csv.reader(election_csv, delimiter=',')
+
+    next(csvreader)
+    pypoll=list(csvreader)
+    row_s = len(pypoll)
+    can_list=list()
+    can_count=list()
+
+    for i in range(0,row_s):
+        candidates = pypoll[i][2]
+        can_count.append(candidates)
+
+        if candidates not in can_list:
+            can_list.append(candidates)
+
+    poll_count=len(can_list)
+
+    percent= list()
+    votes= list()
+
+    for j in range (0,poll_count):
+      name=can_list[j]
+      votes.append(can_count.count(name))
+      vote_p=votes[j]/row_s
+      percent.append(vote_p)
+
+    can_win=votes.index(max(votes))
+
+print('Election Results')
+print('-------------------------')
+print(f'Total Votes:{row_s:,}')
+print('-------------------------')
+for m in range (0,poll_count):
+    print(f'{poll_count[m]} : {percent[m]} ({votes[m]:,})')
+print('-------------------------')
+print(f'Winner: {poll_count[can_win]}')
+print('-------------------------')
+
+election_results = open("election_results.txt","w")
+
+election_results.write('Election Results\n')
+election_results.write('-------------------------\n')
+election_results.write(f'Total Votes:{row_s:,}\n')
+election_results.write('-------------------------\n')
+for m in range (0,poll_count):
+    election_results.write(f'{poll_count[m]} : {percent[m]} ({votes[m]:,})\n')
+election_results.write('-------------------------\n')
+election_results.write(f'Winner: {poll_count[can_win]}\n')
+election_results.write('-------------------------\n')
+
